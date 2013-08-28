27da538fc3aba4f727c4380c55287303406f7fc9
Nexus7 <rickbehl@hotmail.com>
2013-08-29 00:15:31 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyShopifyPgApp::Application.config.secret_key_base = 'a8f96d7d9c314305199df4b23b28f0b0f87d8cfa58eff3e8c7a1ca50410fdf908d488e931df95b6a906f8bca1cd449a9254da770a86007068f068673c021ba71'
